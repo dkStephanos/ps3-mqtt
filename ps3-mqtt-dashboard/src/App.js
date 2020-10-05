965a17f65d86556fa4b70a774eda23db469febd7
dkStephanos <stephanos@goldmail.etsu.edu>
2020-10-05 14:42:10 -0400

    Got basic dashboard running --- not sure if its listening to events yet

--- ps3-mqtt-dashboard/src/App.js
+++ ps3-mqtt-dashboard/src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, Fragment } from 'react';
 import './App.css';
 
+import mqtt from "mqtt";
+
+var options = {
+    port: '1833',
+    host: '192.168.1.107',
+    username: 'koiserve',
+    password: 'Tarnoff',
+};
+var client  = mqtt.connect(options);
+
+client.on('connect', function () {
+  client.subscribe('sub/ack', function (err) {
+    if (!err) {
+      client.publish('sub/ack', 'Hello mqtt')
+    }
+  })
+})
+
+
 function App() {
+  var note;
+  client.on('message', function (topic, message) {
+    note = message.toString();
+    // Updates React state with message 
+    setMesg(note);
+    console.log(note);
+    client.end();
+    });
+
+  // Sets default React state 
+  const [mesg, setMesg] = useState(<Fragment><em>nothing heard</em></Fragment>);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>A taste of MQTT in React</h1>
+        <p>The message is: {mesg}</p>
       </header>
     </div>
   );
