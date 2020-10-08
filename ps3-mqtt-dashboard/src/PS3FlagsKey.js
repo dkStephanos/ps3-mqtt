5db77488418ac74c9b59a303009df813af896822
dkStephanos <stephanos@goldmail.etsu.edu>
2020-10-08 14:34:52 -0400

    Formatted table, inserted data for the key

--- ps3-mqtt-dashboard/src/PS3FlagsKey.js
+++ ps3-mqtt-dashboard/src/PS3FlagsKey.js
@@ -4,11 +4,9 @@ import { useTable } from 'react-table'
 
 const Styles = styled.div`
   padding: 1rem;
-
   table {
     border-spacing: 0;
     border: 1px solid black;
-
     tr {
       :last-child {
         td {
@@ -19,6 +17,7 @@ const Styles = styled.div`
 
     th,
     td {
+      font-size: 12px !important;
       margin: 0;
       padding: 0.5rem;
       border-bottom: 1px solid black;
@@ -46,7 +45,7 @@ function Table({ columns, data }) {
 
   // Render the UI for your table
   return (
-    <table {...getTableProps()}>
+    <table style={{tableLayout: 'auto', width: '100px'}} {...getTableProps()}>
       <thead>
         {headerGroups.map(headerGroup => (
           <tr {...headerGroup.getHeaderGroupProps()}>
@@ -76,36 +75,103 @@ function PS3FlagsKey() {
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Name',
+        Header: 'Directional Flags',
         columns: [
           {
-            Header: 'First Name',
-            accessor: 'firstName',
+            Header: 'Offset 7',
+            accessor: 'directional-of-7',
           },
           {
-            Header: 'Last Name',
-            accessor: 'lastName',
+            Header: 'Offset 6',
+            accessor: 'directional-of-6',
+          },
+          {
+            Header: 'Offset 5',
+            accessor: 'directional-of-5',
+          },
+          {
+            Header: 'Offset 4',
+            accessor: 'directional-of-4',
+          },
+          {
+            Header: 'Offset 3',
+            accessor: 'directional-of-3',
+          },
+          {
+            Header: 'Offset 2',
+            accessor: 'directional-of-2',
+          },
+          {
+            Header: 'Offset 1',
+            accessor: 'directional-of-1',
+          },
+          {
+            Header: 'Offset 0',
+            accessor: 'directional-of-0',
           },
         ],
       },
       {
-        Header: 'Info',
+        Header: 'Named Flags',
         columns: [
           {
-            Header: 'Age',
-            accessor: 'age',
+            Header: 'Offset 7',
+            accessor: 'named-of-7',
           },
           {
-            Header: 'Visits',
-            accessor: 'visits',
+            Header: 'Offset 6',
+            accessor: 'named-of-6',
           },
           {
-            Header: 'Status',
-            accessor: 'status',
+            Header: 'Offset 5',
+            accessor: 'named-of-5',
           },
           {
-            Header: 'Profile Progress',
-            accessor: 'progress',
+            Header: 'Offset 4',
+            accessor: 'named-of-4',
+          },
+          {
+            Header: 'Offset 3',
+            accessor: 'named-of-3',
+          },
+          {
+            Header: 'Offset 2',
+            accessor: 'named-of-2',
+          },
+          {
+            Header: 'Offset 1',
+            accessor: 'named-of-1',
+          },
+          {
+            Header: 'Offset 0',
+            accessor: 'named-of-0',
+          },
+        ],
+      },
+      {
+        Header: 'Left Analog',
+        columns: [
+          {
+            Header: 'Vertical Axis',
+            accessor: 'left-vert-axis',
+          },
+          {
+            Header: 'Horizontal Axis',
+            accessor: 'left-horiz-axis',
+          },
+        ],
+      },
+      ,
+      {
+        Header: 'Right Analog',
+        columns: [
+          {
+            Header: 'Vertical Axis',
+            accessor: 'right-vert-axis',
+          },
+          {
+            Header: 'Horizontal Axis',
+            accessor: 'right-horiz-axis',
           },
         ],
       },
@@ -113,9 +179,37 @@ function PS3FlagsKey() {
     []
   )
 
+   const data = React.useMemo(
+	   () => [
+	     {
+	       'directional-of-7': 'Left',
+	       'directional-of-6': 'Down',
+	       'directional-of-5': 'Right',
+	       'directional-of-4': 'Up',
+	       'directional-of-3': 'Start',
+	       'directional-of-2': 'R3',
+	       'directional-of-1': 'L3',
+	       'directional-of-0': 'Select',
+	       'named-of-7': 'Square',
+	       'named-of-6': 'X',
+	       'named-of-5': 'Circle',
+	       'named-of-4': 'Triangle',
+	       'named-of-3': 'R1',
+	       'named-of-2': 'L1',
+	       'named-of-1': 'R2',
+	       'named-of-0': 'L2',
+	       'left-vert-axis': 'Center ~135',
+	       'left-horiz-axis': 'Center ~125',
+	       'right-vert-axis': 'Center ~135',
+	       'right-horiz-axis': 'Center ~125',
+	     },
+	   ],
+	   []
+	 )
+
   return (
     <Styles>
-      <Table columns={columns} data={['Square', 'Circle']} />
+      <Table columns={columns} data={data} />
     </Styles>
   )
 }
